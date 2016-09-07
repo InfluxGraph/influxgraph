5721413c106b950c2c597e7abeddf0fe997cabc7
Dan <22e889d8@opayq.com>
2016-09-07 16:56:20 +0100

    Updated index tree unittests

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,18 +1,11 @@
 import unittest
-from graphite_influxdb.classes.tree import IndexTree
+from graphite_influxdb.classes.tree import NodeTreeIndex
 from graphite_influxdb.utils import Query
 import datetime
 
 class IndexTreeTestCase(unittest.TestCase):
 
     def setUp(self):
-        # all_series = [
-        #     'root.branch%s.branch%s.branch%s.branch%s.leaf%s' % (b1, b2, b3, b4, n,)
-        #     for b1 in xrange(1,5)
-        #     for b2 in xrange(1,5)
-        #     for b3 in xrange(1,5)
-        #     for b4 in xrange(1,5)
-        #     for n in xrange(1,5)]
         all_series = ['b1.b1.b1.b1.leaf1',
                       'b1.b1.b1.b2.leaf1',
                       'b1.b1.b2.b2.leaf1',
@@ -20,10 +13,41 @@ class IndexTreeTestCase(unittest.TestCase):
                       'b1.b1.b1.b2.leaf2',
                       'b1.b1.b2.b2.leaf2'
                       ]
-        self.index = IndexTree(all_series, None)
+        self.index = NodeTreeIndex()
+        for serie in all_series:
+            self.index.insert(serie)
     
     def test_root_wildcard(self):
-        query = '*'
-        result = list(self.index.search(query))
-        print result
-        import ipdb; ipdb.set_trace()
+        result = list(self.index.query('*'))
+        self.assertTrue(len(result) == 1)
+        self.assertTrue(result[0]['metric'] == 'b1')
+        result = list(self.index.query('b1.*'))
+        self.assertTrue(len(result) == 1)
+        self.assertTrue(result[0]['metric'] == 'b1.b1')
+        result = list(self.index.query('b1.b1.*'))
+        self.assertTrue(len(result) == 2)
+        self.assertTrue(result[0]['metric'] == 'b1.b1.b1')
+        self.assertTrue(result[1]['metric'] == 'b1.b1.b2')
+        result = list(self.index.query('b1.b1.*.*'))
+        self.assertTrue(len(result) == 3)
+        self.assertTrue(result[0]['metric'] == 'b1.b1.b1.b1')
+        self.assertTrue(result[1]['metric'] == 'b1.b1.b1.b2')
+        self.assertTrue(result[2]['metric'] == 'b1.b1.b2.b2')
+        result = list(self.index.query('b1.b1.*.*.*'))
+        self.assertTrue(len(result) == 6)
+        self.assertTrue(result[0]['metric'] == 'b1.b1.b1.b1.leaf1')
+        self.assertTrue(result[1]['metric'] == 'b1.b1.b1.b1.leaf2')
+        self.assertTrue(result[2]['metric'] == 'b1.b1.b1.b2.leaf1')
+        self.assertTrue(result[3]['metric'] == 'b1.b1.b1.b2.leaf2')
+        self.assertTrue(result[4]['metric'] == 'b1.b1.b2.b2.leaf1')
+        self.assertTrue(result[5]['metric'] == 'b1.b1.b2.b2.leaf2')
+        result = list(self.index.query('b1.b1.*.*.{leaf1,leaf2}'))
+        self.assertTrue(len(result) == 6)
+        self.assertTrue(result[0]['metric'] == 'b1.b1.b1.b1.leaf1')
+        self.assertTrue(result[1]['metric'] == 'b1.b1.b1.b1.leaf2')
+        self.assertTrue(result[2]['metric'] == 'b1.b1.b1.b2.leaf1')
+        self.assertTrue(result[3]['metric'] == 'b1.b1.b1.b2.leaf2')
+        self.assertTrue(result[4]['metric'] == 'b1.b1.b2.b2.leaf1')
+        self.assertTrue(result[5]['metric'] == 'b1.b1.b2.b2.leaf2')
+        result = list(self.index.query('fakey*'))
+        self.assertFalse(result)
