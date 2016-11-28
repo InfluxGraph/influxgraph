0f172f3890bef0d6dfe215a61a15a0682dc6bd4d
Dan <22e889d8@opayq.com>
2016-11-28 12:43:28 +0000

    Made C templates test py2.6 and pypy compatible

--- tests/test_c_templates.py
+++ tests/test_c_templates.py
@@ -1,5 +1,8 @@
 import unittest
-from influxgraph.ext.templates import parse_series
+try:
+    from influxgraph.ext.templates import parse_series
+except ImportError:
+    from influxgraph.templates import parse_series
 from string import ascii_letters
 from random import choice
 from influxgraph.templates import _parse_influxdb_graphite_templates
@@ -11,7 +14,9 @@ class TemplatesCExtTestCase(unittest.TestCase):
         self.paths = [u'test_type', u'host']
         self.measurements = ['cpu', 'memory', 'load', 'iops']
         self.field_names = [u'field1', u'field2']
-        self.all_fields = {m: self.field_names for m in self.measurements}
+        self.all_fields = {}
+        for m in self.measurements:
+            self.all_fields[m] = self.field_names
         self.tags = {
             # Tags parsed from metric path
             self.paths[0]: self.metric_prefix,
@@ -33,7 +38,9 @@ class TemplatesCExtTestCase(unittest.TestCase):
         measurements = [''.join([choice(ascii_letters) for _ in range(10)]) for _ in range(10)]
         series = [u'%s,a=1,b=2,c=3,d=4,e=5,f=6,g=7,m=8,n=9,j=10' % (m,) for m in measurements]
         field_names = [u'f1', u'f2', u'f3', u'f4', u'f5', u'f6', u'f7', u'f8', u'f9', u'f10']
-        fields = {m: [u'f1', u'f2', u'f3', u'f4', u'f5', u'f6', u'f7', u'f8', u'f9', u'f10'] for m in measurements}
+        fields = {}
+        for m in measurements:
+            fields[m] = [u'f1', u'f2', u'f3', u'f4', u'f5', u'f6', u'f7', u'f8', u'f9', u'f10']
         templates = _parse_influxdb_graphite_templates(
             ["a.b.c.d.e.f.g.m.n.j.measurement.field* env=int,region=the_west"])
         index = parse_series(series, fields, templates)
