ddfaab2e1156164cf0d38e7a1c526bf8709d39c4
Dan <22e889d8@opayq.com>
2016-11-28 15:13:43 +0000

    Added handling index perf test on platforms that do not support C extension

--- tests/templates_parse_perf.py
+++ tests/templates_parse_perf.py
@@ -12,7 +12,7 @@ series = [u'%s,a=1,b=2,c=3,d=4,e=6,f=7,g=8,m=9,n=10,j=11' % (m,) for m in measur
 fields = {m: [u'f1', u'f2', u'f3', u'f4', u'f5', u'f6', u'f7', u'f8', u'f9', u'f10'] for m in measurements}
 templates = _parse_influxdb_graphite_templates(["a.b.c.d.e.f.g.m.n.j.measurement.field* env=int,region=the_west"])
     """
-    
+
     def test_python_template(self):
         setup = '\n'.join(["from influxgraph.templates import parse_series",
                            self.timeit_setup])
@@ -21,7 +21,12 @@ templates = _parse_influxdb_graphite_templates(["a.b.c.d.e.f.g.m.n.j.measurement
         pprint("Python template parse time is %s" % (parse_time,))
 
     def test_cython_template(self):
-        setup = '\n'.join(["from influxgraph.ext.templates import parse_series",
+        setup = '\n'.join(["""
+try:
+    from influxgraph.ext.templates import parse_series
+except ImportError:
+    from influxgraph.templates import parse_series
+""",
                            self.timeit_setup])
         parse_time = timeit(
             stmt=self.template_stmt, setup=setup, number=100)
