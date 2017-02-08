5e8b6d04da5e899b6d3da35a169ad15d4ae643fb
Dan <22e889d8@opayq.com>
2017-02-08 13:08:10 +0000

    Updated loader limit default to be 10k - influxdb 1.2 silently truncates series lists over 10k

--- influxgraph/constants.py
+++ influxgraph/constants.py
@@ -34,7 +34,7 @@ DEFAULT_AGGREGATIONS = {r'\.min$' : 'min',
 _INFLUXDB_CLIENT_PARAMS = {'epoch': 's'}
 SERIES_LOADER_MUTEX_KEY = 'influxgraph_series_loader'
 MEMCACHE_SERIES_DEFAULT_TTL = 1800
-LOADER_LIMIT = 100000
+LOADER_LIMIT = 10000
 # Fill params like 'null' that leave gaps are not valid for use with Graphite
 # API and are purposefully not included here
 FILL_PARAMS = ['previous', 'linear']
