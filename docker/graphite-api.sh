f107c030084d66b7246336121a9cc43c09e7bb27
Dan <22e889d8@opayq.com>
2017-05-14 20:11:13 +0100

    Switched docker container to CPython. Updated dockerfile to build influxgraph native extensions

--- docker/graphite-api.sh
+++ docker/graphite-api.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-VIRTUAL_ENV=/srv/graphite-pypy
-PATH=/srv/graphite-pypy/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 exec /sbin/setuser root gunicorn -b 0.0.0.0:8000 -w 2 --timeout 120 --log-level debug graphite_api.app:app >>/var/log/graphite-api.log 2>&1
--- docker/nginx.sh
+++ docker/nginx.sh
@@ -3,6 +3,5 @@
 if [ -r /etc/default/nginx ]; then
         . /etc/default/nginx
 fi
-PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 DAEMON=/usr/sbin/nginx
 $DAEMON > /dev/null
