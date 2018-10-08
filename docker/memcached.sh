ea4a746ddadd45c3cadee8f3ffb1d849e585ac35
tux1337 <33804893+tux1337@users.noreply.github.com>
2018-10-08 14:29:22 +0200

    Docker: make memcache configurable, Update Baseimage to v0.10 (#54)
    * memcache.conf support in memcache.sh startup script added
    * example memcache.conf file added (it differs a little bit from the original one from the debian package: -d and logfile is not supported for architecture reasons)
    * Update to phusion/baseimage:0.10.0 docker base image

--- docker/memcached.sh
+++ docker/memcached.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 # `/sbin/setuser memcache` runs the given command as the user `memcache`.
 # If you omit that part, the command will be run as root.
-exec /sbin/setuser memcache /usr/bin/memcached >>/var/log/memcached.log 2>&1
+
+#read conf file
+options=$(cat /etc/memcached.conf | grep -v '#' | grep -v '^$' | tr '\n' ' ')
+
+exec /usr/bin/memcached $options >>/var/log/memcached.log 2>&1
